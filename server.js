823e7424fcadd96eca74d5da057234f9090577cb
aruljobin <aruljobin@gmail.com>
2017-08-19 18:18:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/artical-one',function(req,res){
-   res.send('Hi this is artical-one'); 
+    res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
